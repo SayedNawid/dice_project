dac36b3fc9e447acc2a79933fc3197e7238d78e2
SayedNawid <sayednawidmohammady@gmail.com>
2023-04-09 22:38:04 +0430

    finished project

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,63 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.cyan.shade800,
-        appBar: AppBar(
-          title: Text('Dice'),
-          backgroundColor: Colors.cyan.shade900,
-        ),
-        body: DicePage(),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.cyan.shade800,
+      appBar: AppBar(
+        title: Text("Dice"),
+        backgroundColor: Colors.cyan.shade900,
       ),
+      body: DicePage(),
     ),
-  );
+  ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftCountBtn = 1;
+  int rightCountBtn = 1;
+
+  void callBack() {
+    setState(() {
+      leftCountBtn = Random().nextInt(6) + 1;
+      rightCountBtn = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                callBack();
+              },
+              child: Image.asset('images/dice$leftCountBtn.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  callBack();
+                });
+              },
+              child: Image.asset("images/dice$rightCountBtn.png"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
